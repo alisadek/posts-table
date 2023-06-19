dbe1342cff7ee1ac52c1ab2859291ec43cd04bb3
Ali Sadek <ali.sadek@qpr.com>
2023-06-19 08:34:05 +0300

    feat: add skeleton. re-organize code

--- src/components/PostsTable/PostsTable.tsx
+++ src/components/PostsTable/PostsTable.tsx
@@ -11,6 +11,7 @@ import {
   TablePagination,
   TableRow,
   TextField,
+  Skeleton,
   InputAdornment,
   Box,
 } from "@mui/material";
@@ -21,6 +22,7 @@ import { RootState } from "../../store/store";
 import { deletePost, fetchPosts } from "../../store/posts/postsActions";
 import DeleteModal from "../DeleteModal/DeleteModal";
 import { debounce } from "lodash";
+
 type Props = {};
 
 const PostsTable = (props: Props) => {
@@ -29,8 +31,13 @@ const PostsTable = (props: Props) => {
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
   const [postIdToDelete, setPostIdToDelete] = useState<number | null>(null);
   const navigate = useNavigate();
-  const [searchQuery, setSearchQuery] = useState('');
-  
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const dispatch = useDispatch();
+  const { data: posts, loading, error } = useSelector(
+    (state: RootState) => state.posts
+  );
+
   const handleChangePage = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null,
     newPage: number
@@ -38,23 +45,18 @@ const PostsTable = (props: Props) => {
     setPage(newPage);
   };
 
-  const handleChangeRowsPerPage:
-    | React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>
-    | undefined = (event) => {
-    setRowsPerPage(+event?.target.value);
+  const handleChangeRowsPerPage: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    setRowsPerPage(+event.target.value);
     setPage(0);
   };
-  const dispatch = useDispatch();
-  const {
-    data: posts,
-    loading,
-    error,
-  } = useSelector((state: RootState) => state.posts);
 
   const handleDeleteClick = (postId: number) => {
     setPostIdToDelete(postId);
     setDeleteModalOpen(true);
   };
+
   const handleDeleteConfirm = () => {
     if (postIdToDelete) {
       dispatch(deletePost(postIdToDelete));
@@ -67,10 +69,14 @@ const PostsTable = (props: Props) => {
     setDeleteModalOpen(false);
     setPostIdToDelete(null);
   };
-  const handleSearchChange: React.ChangeEventHandler<HTMLInputElement> =  ( e ) => {
+
+  const handleSearchChange: React.ChangeEventHandler<HTMLInputElement> = (
+    e
+  ) => {
     const query = e.target.value;
     setSearchQuery(query);
   };
+
   useEffect(() => {
     const debouncedSearch = debounce((query) => {
       dispatch(fetchPosts(query));
@@ -82,10 +88,14 @@ const PostsTable = (props: Props) => {
       debouncedSearch.cancel();
     };
   }, [searchQuery, dispatch]);
+
   useEffect(() => {
-    dispatch(fetchPosts(''));
+    dispatch(fetchPosts(""));
   }, [dispatch]);
-  if (error) return <div>{error}</div>;
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <StyledPostsPage>
@@ -111,52 +121,77 @@ const PostsTable = (props: Props) => {
           sx={{ alignSelf: "end", width: "300px" }}
           placeholder="Search..."
         />
-        {
-          loading ?<div>Loading...</div>:
+
+        {loading ? (
+          <Box sx={{ display: "flex", flexDirection: "column", gap: "15px" }}>
+            <Skeleton variant="rectangular" height={56} />
+            {[...Array(rowsPerPage)].map((_, index) => (
+              <Skeleton key={index} variant="rectangular" height={56} />
+            ))}
+          </Box>
+        ) : (
           <>
-        <TableContainer sx={{ maxHeight: 440 }}>
-          <StyledTable stickyHeader>
-            <TableHead>
-              <TableRow>
-                <TableCell align="left">Title</TableCell>
-                <TableCell align="center">Description</TableCell>
-                <TableCell align="center">Actions</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {posts
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((post) => (
-                  <TableRow key={post.id}>
-                    <TableCell align="left">{post.title}</TableCell>
-                    <TableCell align="center">{post.body}</TableCell>
-                    <TableCell align="center">
-                      <IconButton onClick={() => handleDeleteClick(post.id)}>
-                        <DeleteIcon />
-                      </IconButton>
-                      <IconButton onClick={() => navigate(`/${post.id}`)}>
-                        <EditIcon />
-                      </IconButton>
+            <TableContainer sx={{ maxHeight: 440 }}>
+              <StyledTable stickyHeader>
+                <TableHead>
+                  <TableRow>
+                    <TableCell sx={{ maxWidth: "100px" }} align="left">
+                      Title
                     </TableCell>
+                    <TableCell sx={{ maxWidth: "600px" }} align="left">
+                      Description
+                    </TableCell>
+                    <TableCell align="center">Actions</TableCell>
                   </TableRow>
-                ))}
-            </TableBody>
-          </StyledTable>
-        </TableContainer>
-        <TablePagination
-          sx={{ px: 2 }}
-          page={page}
-          component="div"
-          rowsPerPage={rowsPerPage}
-          count={posts.length}
-          onPageChange={handleChangePage}
-          rowsPerPageOptions={[5, 10, 25]}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-          nextIconButtonProps={{ "aria-label": "Next Page" }}
-          backIconButtonProps={{ "aria-label": "Previous Page" }}
-        />
-        </>
-        }
+                </TableHead>
+                <TableBody>
+                  {posts
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((post) => (
+                      <TableRow key={post.id}>
+                        <TableCell
+                          width="100px"
+                          sx={{ maxWidth: "100px" }}
+                          align="left"
+                        >
+                          {post.title}
+                        </TableCell>
+                        <TableCell
+                          width="600px"
+                          sx={{ maxWidth: "600px" }}
+                          align="left"
+                        >
+                          {post.body}
+                        </TableCell>
+                        <TableCell align="center">
+                          <IconButton
+                            onClick={() => handleDeleteClick(post.id)}
+                          >
+                            <DeleteIcon />
+                          </IconButton>
+                          <IconButton onClick={() => navigate(`/${post.id}`)}>
+                            <EditIcon />
+                          </IconButton>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+              </StyledTable>
+            </TableContainer>
+            <TablePagination
+              sx={{ px: 2 }}
+              page={page}
+              component="div"
+              rowsPerPage={rowsPerPage}
+              count={posts.length}
+              onPageChange={handleChangePage}
+              rowsPerPageOptions={[5, 10, 25]}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              nextIconButtonProps={{ "aria-label": "Next Page" }}
+              backIconButtonProps={{ "aria-label": "Previous Page" }}
+            />
+          </>
+        )}
       </Box>
       <DeleteModal
         open={deleteModalOpen}
